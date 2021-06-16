f18913e8eddb4580fa8cf0ab1e06ec061e285c4e
Vitaly Myznikov <apxuk@yandex.ru>
2021-06-16 12:28:04 +0300

    fix

--- task_1.py
+++ task_1.py
@@ -6,9 +6,8 @@ events_df = pd.read_csv('data_engineer_task.csv', sep=';', parse_dates=['timesta
 
 events_df.sort_values(['uid', 'timestamp'], inplace=True)
 events_df['delta'] = events_df.groupby('uid')['timestamp'].diff()
-#
-#
-# events_df['session'] = events_df['uid'].astype(str) + '_' + (events_df['delta'] > pd.Timedelta(minutes=30)).cumsum().astype(str)
-events_df['session'] = (events_df['delta'] > pd.Timedelta(minutes=30)).cumsum()
+
+events_df['session'] = events_df['uid'].astype(str) + '_' + (events_df['delta'] > pd.Timedelta(minutes=30)).cumsum().astype(str)
 
 events_df.to_csv('events_with_sessions.csv', sep=';', index=False)
+
